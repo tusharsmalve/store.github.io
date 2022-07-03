e13bae85d982e4aa0a80bbb117631409a37894ed
tusharsmalve <87444862+tusharsmalve@users.noreply.github.com>
2022-07-03 21:18:55 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,48 @@
+$(document).ready(function () {
+  var noOfProduct = $("#product_container ul li").length;
+  var totalProductWidth = 0;
+  for (var i = 0; i < noOfProduct; i++) {
+        var productWidth = $("#product_container ul li").eq(i).outerWidth(true);
+        totalProductWidth = totalProductWidth + productWidth;
+  }
+
+  $("#slider").css("width", totalProductWidth + "px");
+
+  var speed = 1;
+  animateProducts();
+
+  function animateProducts() {
+        $("#slider li")
+              .eq(0)
+              .animate(
+                    {
+                          marginLeft: "-=" + speed + "px"
+                    },
+                    1,
+                    function () {
+                          var animProductWidth = $(this).outerWidth(true);
+
+                          if (speed >= animProductWidth) {
+                                $(this).parent().append($(this));
+                                $(this).removeAttr("style");
+                          }
+
+                          aninInterval = setTimeout(function () {
+                                animateProducts();
+                          });
+                    }
+              );
+  }
+
+  $("#slider").hover(
+        function () {
+              clearTimeout(aninInterval);
+              $("#slider li").eq(0).stop();
+        },
+        function () {
+              animateProducts();
+        }
+  );
+
+
+});
